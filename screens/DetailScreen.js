c2036763343ddf5419bd3c32e03a7363594c26da
slo-ow <icet25@naver.com>
2022-02-23 19:20:05 +0900

    react-navigation

--- screens/DetailScreen.js
+++ screens/DetailScreen.js
@@ -1,12 +1,40 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useEffect} from 'react';
+import {View, Text, StyleSheet, Button} from 'react-native';
 
-function DetailScreen() {
+function DetailScreen({route, navigation}) {
+  useEffect(() => {
+    navigation.setOptions({
+      title: `상세 정보 - ${route.params.id}`,
+    });
+  }, [navigation, route.params.id]);
+
   return (
-    <View>
-      <Text>Detail</Text>
+    <View style={styles.block}>
+      <Text style={styles.text}>id: {route.params.id}</Text>
+      <View style={styles.buttons}>
+        <Button
+          title="다음"
+          onPress={() => navigation.push('Detail', {id: route.params.id + 1})}
+        />
+        <Button title="뒤로가기" onPress={() => navigation.pop()} />
+        <Button title="처음으로" onPress={() => navigation.popToTop()} />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 48,
+  },
+  buttons: {
+    flexDirection: 'row',
+  },
+});
 
 export default DetailScreen;
