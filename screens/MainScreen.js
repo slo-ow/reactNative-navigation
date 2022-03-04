16c10d4bfc510c5606be8bc4204a03ef900a8089
slo-ow <icet25@naver.com>
2022-03-04 22:02:24 +0900

    하단 탭 구성

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
 import {Text, View, Button} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-const Tab = createMaterialTopTabNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
 function HomeScreen({navigation}) {
   return (
@@ -50,10 +50,7 @@ function MainScreen() {
     <Tab.Navigator
       initialRouteName="Home"
       screenOptions={{
-        tabBarIndicatorStyle: {
-          backgroundColor: '#009688',
-        },
-        tabBarActiveTintColor: '#009688',
+        showIcon: true,
       }}>
       <Tab.Screen
         name="Home"
@@ -61,6 +58,8 @@ function MainScreen() {
         options={{
           tabBarLabel: '홈',
           tabBarIcon: ({color}) => <Icon name="home" color={color} size={24} />,
+          tabBarColor: 'black',
+          tabBarBadge: 'new',
         }}
       />
       <Tab.Screen
@@ -71,6 +70,7 @@ function MainScreen() {
           tabBarIcon: ({color}) => (
             <Icon name="search" color={color} size={24} />
           ),
+          tabBarColor: 'green',
         }}
       />
       <Tab.Screen
@@ -81,6 +81,8 @@ function MainScreen() {
           tabBarIcon: ({color}) => (
             <Icon name="notifications" color={color} size={24} />
           ),
+          tabBarColor: 'gray',
+          tabBarBadge: 30,
         }}
       />
       <Tab.Screen
@@ -91,6 +93,8 @@ function MainScreen() {
           tabBarIcon: ({color}) => (
             <Icon name="message" color={color} size={24} />
           ),
+          tabBarColor: 'blue',
+          tabBarBadge: true,
         }}
       />
     </Tab.Navigator>
